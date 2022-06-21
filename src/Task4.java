3ddec45e6dba79cc203c076e7671535ee879af5d
roman <roman.korneev@inbox.ru>
2022-06-21 21:41:43 +0300

    add hanoy tower

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,15 @@
 public class Task4 {
+
+    public static void hanooyB(char from, char buf, char to, int n){
+        if(n == 1) System.out.println("from " + from + " to " + buf);
+        else {
+            hanooyB(from, to, buf, n - 1);
+            System.out.println("from " + from + " to " + buf);
+            hanooyB(to, buf, from, n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        hanooyB('a', 'b', 'c', 3);
+    }
 }
